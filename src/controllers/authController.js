ecc457eaee68b65b72fd39844227e0e4c2650ade
yJeanx11x <j3anlucas010@gmail.com>
2026-05-22 16:24:23 -0300

    refactor: ajusta formatação e corrige hash da senha no controlador de autenticação

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,8 @@
 // Banco de dados
-const {User} = require('../models/user')
+const { User } = require('../models/user')
 const trasaction = require('../models/Transaction')
-
+// criptografia de senha 
+const bcrypt = require('bcrypt')
 // Autentificação zod
 const userSchemas = require('../schemas/userSchema')
 
@@ -9,21 +10,22 @@ const userSchemas = require('../schemas/userSchema')
 async function register(req, res, next) {
     const { nome, email, password } = req.body
     const z = userSchemas.safeParse(req.body)
-    
+
+    const senhaHash = await bcrypt.hash(password, 12)
+
     try {
-    
-       await User.create({
+        await User.create({
             nome: z.data.nome,
             email: z.data.email,
-            password: z.data.password
+            password: senhaHash
         })
         return res.status(200).json({ message: 'Usuário criado com sucesso' })
 
     } catch (error) {
         next(error)
-        
+
     }
 
 }
 
-module.exports = {register} 
+module.exports = { register } 
