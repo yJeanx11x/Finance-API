155d838c0bd0dae7e93409a7ec55b4d8d5139196
yJeanx11x <j3anlucas010@gmail.com>
2026-05-24 08:38:32 -0300

    feat: adiciona função de login ao controlador de autenticação

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,6 +7,7 @@ const bcrypt = require('bcrypt')
 const userSchemas = require('../schemas/userSchema')
 
 
+// Função de registro de usuário
 async function register(req, res, next) {
     const { nome, email, password } = req.body
     const z = userSchemas.safeParse(req.body)
@@ -28,4 +29,32 @@ async function register(req, res, next) {
 
 }
 
-module.exports = { register } 
+async function login(req, res, next) {
+    const { email, password } = req.body
+
+    try {
+        const userDb = await User.findOne({ where: { email } })
+        const comparePassword = await bcrypt.compare(password,userDb.password)
+
+        if (!userDb) {
+            return res.status(404).json({ message: 'email não cadastrado' })
+        }
+        if (!comparePassword){
+            return res.status(404).json({ message: 'Senha incorreta' })
+
+        }
+
+
+            return res.status(200).json({ message: 'logado' })
+
+    } catch (error) {
+        next(error)
+    }
+
+
+
+}
+
+
+
+module.exports = { register, login } 
