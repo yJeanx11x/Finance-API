a627406b90dc5b5d91237d9cc8fdcec292a6f1c2
yJeanx11x <j3anlucas010@gmail.com>
2026-05-28 15:56:51 -0300

    fix: corrige rotas de transações para consistência na nomenclatura

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -11,13 +11,12 @@ appRoutes.use(limiter)
 
 
 appRoutes.post('/register', authController.register)
-
 appRoutes.post('/login', authController.login)
 
 
 
-appRoutes.get('/transactions', jwt.verificarToken, transactionController.transactions)
-appRoutes.post('/newTransaction', jwt.verificarToken, transactionController.newTransaction)
+appRoutes.get('/transaction', jwt.verificarToken, transactionController.transactions)
+appRoutes.post('/transaction', jwt.verificarToken, transactionController.newTransaction)
 appRoutes.patch('/transaction/:id', jwt.verificarToken, transactionController.editTransaction)
 appRoutes.delete('/transaction/:id', jwt.verificarToken, transactionController.deleteTransaction)
 
